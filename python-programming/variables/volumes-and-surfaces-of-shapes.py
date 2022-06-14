0b7c11dc38c2de1f49b8b2ecee9b522b10017ed8
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-14 22:24:29 +0100

    Imports math and defines variables

--- python-programming/variables/volumes-and-surfaces-of-shapes.py
+++ python-programming/variables/volumes-and-surfaces-of-shapes.py
@@ -1,1 +1,15 @@
+import math
 
+# You have shapes with measurements:
+# Cuboid: Height (h): 25/7, Width (w): 25/2, Length (l): 35
+# Cone: Height (h): 10, Radius of base (r): 3
+#Calculate the volume and surface areas of the shapes using the following equations.
+h = 25/7
+w = 25/2
+l = 35
+h_2 = 10
+r = 3
+pi = 3.14
+
+cuboid = "cuboid"
+cone = "cone"
