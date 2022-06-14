b8a5482f9aea8ad5d0693988d1b6ad37d2ddc733
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-14 21:46:20 +0100

    Defines variables

--- python-programming/variables/calculate-your-shopping-bills.py
+++ python-programming/variables/calculate-your-shopping-bills.py
@@ -4,7 +4,11 @@
 # Washing sponges cost 29p for a pack of 10
 # Juice is £1.89 per 1.5l bottle
 # Foil is £1.29 per 30m roll
-
+six_pack_tomatoes = 0.87
+half_kg_sugar = 1.09
+ten_pack_sponges = 0.29
+one_and_half_liter_juice_bottle = 1.89
+thirty_meter_foil_roll = 1.29
 
 # Using arithmetic operations in Python, calculate the price of 1kg sugar
 
