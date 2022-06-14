53e1558382243e1e587275ede1e18b53d6c3f848
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-14 21:43:50 +0100

    Initialises file

--- python-programming/variables/calculate-your-shopping-bills.py
+++ python-programming/variables/calculate-your-shopping-bills.py
@@ -1,1 +1,12 @@
+# Assign variables for each of these items and their prices
+# Tomatoes cost 87p for a pack of 6
+# 500g sugar costs £1.09
+# Washing sponges cost 29p for a pack of 10
+# Juice is £1.89 per 1.5l bottle
+# Foil is £1.29 per 30m roll
 
+
+# Using arithmetic operations in Python, calculate the price of 1kg sugar
+
+
+# What is the price of 20 washing sponges, 3l of juice, and 2 packs of tomatoes?
