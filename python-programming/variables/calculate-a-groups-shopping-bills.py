17ce52b20c5ad41e6a81e19a7b0a62cb08674857
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-14 22:01:20 +0100

    Adds calculations

--- python-programming/variables/calculate-a-groups-shopping-bills.py
+++ python-programming/variables/calculate-a-groups-shopping-bills.py
@@ -17,9 +17,14 @@ thirty_meter_foil_roll = 1.29
 # 20m of foil
 # 180g sugar
 # Assign the value to total
+total = 5 * (six_pack_tomatoes + (ten_pack_sponges / 10) * 3 + (one_and_half_liter_juice_bottle / 3) * 2 + (thirty_meter_foil_roll / 3) * 2 + (half_kg_sugar / 500) * 180)
 
 #Provide your answer as a printed variable called total.
+print(total)
 
 # These prices do not include VAT. Calculate the VAT on the total, assuming that VAT is 20%.
+vat = total * 1.2
+print(vat)
 
 #Then round it appropriately to two decimal places.
+print(round(vat, 2))
