f1f448ba316a34564c429bd3ffea6c49c809f329
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-14 21:40:12 +0100

    Adds two first exercises

--- python-programming/variables/exercises-on-numbers.py
+++ python-programming/variables/exercises-on-numbers.py
@@ -1,1 +1,7 @@
+# Write an expression using addition & subtraction & multiplication & division that is equal to 1044.75
+print(((500 * 8) / 4 + 49.75 - 5))
 
+# Check whether a number is a multiple of 3 using modulo
+print(9 % 3)    # multiple (outputs 0)
+print(10 % 3)   # not a multiple (outputs number != 0)
+print(11 % 3)   # not a multiple 
