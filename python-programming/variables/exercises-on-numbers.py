ac0c1d5a389486cf26d885d2d0ed44162242a268
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-14 21:41:26 +0100

    Adds last exercise

--- python-programming/variables/exercises-on-numbers.py
+++ python-programming/variables/exercises-on-numbers.py
@@ -5,3 +5,7 @@ print(((500 * 8) / 4 + 49.75 - 5))
 print(9 % 3)    # multiple (outputs 0)
 print(10 % 3)   # not a multiple (outputs number != 0)
 print(11 % 3)   # not a multiple 
+
+# Calculate the following expression
+a = 1 + 2 * 3 - 4 / 6 * (6 + 7) * 8 ** 9
+print(a)
