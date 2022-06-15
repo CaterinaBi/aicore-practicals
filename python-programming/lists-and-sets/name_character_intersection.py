eb4fb41f7efe3c8906375d8bf99755674d5b60b8
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-15 21:40:35 +0100

    Adds the required functionalities

--- python-programming/lists-and-sets/name_character_intersection.py
+++ python-programming/lists-and-sets/name_character_intersection.py
@@ -2,3 +2,9 @@
 spouses_names = ["caterina", "vincent"]
 
 # Find the characters that appear in both names
+my_set1 = set(spouses_names[0])
+my_set2 = set(spouses_names[1])
+print(my_set1, my_set2)
+
+common_letters = my_set1.intersection(my_set2)
+print("The shared characters are: ", common_letters)
