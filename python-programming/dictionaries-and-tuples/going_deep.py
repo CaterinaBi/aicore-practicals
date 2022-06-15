925bbfe2a9120375ad569edb85f3c5fbf6e66052
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-15 23:05:19 +0100

    Creates file

---
+++ python-programming/dictionaries-and-tuples/going_deep.py
@@ -0,0 +1,4 @@
+# Use indexing to get the string "Python" from the following dictionary: 
+# one_deep_dictionary = {'start here':1,'k1':[1,2,3,{'k2':[1,2,{'k3':['keep going',{'further':[1,2,3,4,[{'k4':'Python'}]]}]}]}]}
+
+one_deep_dictionary = {'start here':1,'k1':[1,2,3,{'k2':[1,2,{'k3':['keep going',{'further':[1,2,3,4,[{'k4':'Python'}]]}]}]}]}
