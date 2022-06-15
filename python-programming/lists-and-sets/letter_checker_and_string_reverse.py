89fcff1233a3b1b4c89808067347aa437f718183
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-15 20:41:59 +0100

    Solves first problem

--- python-programming/lists-and-sets/letter_checker_and_string_reverse.py
+++ python-programming/lists-and-sets/letter_checker_and_string_reverse.py
@@ -3,6 +3,8 @@
 phrase1 = 'Clean Couch'
 phrase2 = 'Giant Table'
 
+print(phrase1[0] == phrase2[0])
+
 # Write a program that returns the following strings with the words reversed (Not the characters).
 
 my_string1 = 'This is a short phrase'
