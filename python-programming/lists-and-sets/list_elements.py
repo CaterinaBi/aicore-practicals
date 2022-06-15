e87622c35f4c28344c091508e1584fa1501d4cae
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-15 20:49:18 +0100

    Initialises file

--- python-programming/lists-and-sets/list_elements.py
+++ python-programming/lists-and-sets/list_elements.py
@@ -1,1 +1,9 @@
+# Create a list of five elements with a different data type. Name it list_1
 
+# Now create a list with ten zeros, do it using the '*' operator. Name it list_2
+
+# Nest list_1 and list_2 into a new list and name it list_3. So list_3 should look like this [list_1, list2]
+
+# Access the fourth element of each list_1 and list_2
+# store the elements in list_4. 
+# list_4 should look like this [fourth element of list_1, fourth element of list_2]
