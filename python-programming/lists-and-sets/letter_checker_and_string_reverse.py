27573dcaf5bbadd189e15fe7bfd9d54c39add44b
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-15 20:29:32 +0100

    Creates file

--- python-programming/lists-and-sets/letter_checker_and_string_reverse.py
+++ python-programming/lists-and-sets/letter_checker_and_string_reverse.py
@@ -1,1 +1,9 @@
+# Write a program that checks if the two words in the following strings start with the same letter.
 
+phrase1 = 'Clean Couch'
+phrase2 = 'Giant Table'
+
+# Write a program that returns the following strings with the words reversed (Not the characters).
+
+my_string1 = 'This is a short phrase'
+my_string2 = 'This is actually a significantly longer phrase than the previous one'
