8db6ad8c28b15de41217e1c6fd266bbd7052680c
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-15 20:27:39 +0100

    Implements required functions

--- python-programming/lists-and-sets/username_list.py
+++ python-programming/lists-and-sets/username_list.py
@@ -1,9 +1,14 @@
 # Create a list of usernames, and name it usernames.
+usernames = ["caterina", "isadora", "vincent", "leone", "bonan", "rifflart"]
 
 # Print the type of usernames
+print(type(usernames))
 
 # Print the length of usernames
+print(len(usernames))
 
 # Print the type of the first item in usernames
+print(type(usernames[0]))
 
 # Find the length of the last name in usernames
+print(len(usernames[-1]))
