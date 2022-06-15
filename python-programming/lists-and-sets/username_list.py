72ed17fb968bc091cb3764565a1d3737ef0288a4
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-15 20:23:20 +0100

    Creates file

--- python-programming/lists-and-sets/username_list.py
+++ python-programming/lists-and-sets/username_list.py
@@ -1,1 +1,9 @@
+# Create a list of usernames, and name it usernames.
 
+# Print the type of usernames
+
+# Print the length of usernames
+
+# Print the type of the first item in usernames
+
+# Find the length of the last name in usernames
