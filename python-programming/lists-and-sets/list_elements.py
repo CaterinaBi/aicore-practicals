699368b0293685055bb0a11e12d7938dab107f98
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-15 20:58:58 +0100

    Adds calculations

--- python-programming/lists-and-sets/list_elements.py
+++ python-programming/lists-and-sets/list_elements.py
@@ -1,9 +1,19 @@
 # Create a list of five elements with a different data type. Name it list_1
+list_1 = [7, 9, 22, 46, 10]
+print("List 1: ", list_1)
 
 # Now create a list with ten zeros, do it using the '*' operator. Name it list_2
+list_2 = [0] * 10
+print("List 2: ", list_2)
 
 # Nest list_1 and list_2 into a new list and name it list_3. So list_3 should look like this [list_1, list2]
+list_3 = []
+list_3.append(list_1)
+list_3.append(list_2)
+print("List 3: ", list_3)
 
 # Access the fourth element of each list_1 and list_2
 # store the elements in list_4. 
 # list_4 should look like this [fourth element of list_1, fourth element of list_2]
+list_4 = [list_1[3], list_2[3]]
+print("List 4: ", list_4)
