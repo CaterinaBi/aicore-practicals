debdc47d08676dda2b899e55545cce4b961d29c6
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-15 20:46:15 +0100

    Solves second problem

--- python-programming/lists-and-sets/letter_checker_and_string_reverse.py
+++ python-programming/lists-and-sets/letter_checker_and_string_reverse.py
@@ -9,3 +9,13 @@ print(phrase1[0] == phrase2[0])
 
 my_string1 = 'This is a short phrase'
 my_string2 = 'This is actually a significantly longer phrase than the previous one'
+
+split_string1 = my_string1.split()
+print(split_string1)
+split_string1.reverse()
+print(split_string1)
+
+split_string2 = my_string2.split()
+print(split_string2)
+split_string2.reverse()
+print(split_string2)
