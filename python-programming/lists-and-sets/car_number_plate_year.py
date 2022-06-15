b6c27104266c9e61fe221554c80043343d4f1f88
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-15 21:02:36 +0100

    Adds instructions

--- python-programming/lists-and-sets/car_number_plate_year.py
+++ python-programming/lists-and-sets/car_number_plate_year.py
@@ -1,1 +1,7 @@
+# Given the following list of number plates: ["G06 WTR", "WL11 WFL", "QW68 PQR"]
 
+# Extract the last two characters of the first word, which represent the year of the car
+
+# Print the type of each of them
+
+# Convert each of these years to an integer type
