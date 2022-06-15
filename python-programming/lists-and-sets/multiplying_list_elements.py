81f7f2cd790367737885ef9b84c7278a2e45fba4
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-15 21:44:45 +0100

    Implements required fonctionalities

--- python-programming/lists-and-sets/multiplying_list_elements.py
+++ python-programming/lists-and-sets/multiplying_list_elements.py
@@ -1,3 +1,7 @@
 # Create a list with ten zeros, do it using the '*' operator. Name it list_1
+list_1 = [0] * 10
+print(list_1)
 
 # Create a set from list_1 and print its length.
+my_set = set(list_1)
+print("The length of set_1 is", len(my_set), "element(s).")
