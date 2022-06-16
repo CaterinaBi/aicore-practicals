72ae5fc916f974439438950022e8818cf88a4729
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-16 22:02:46 +0100

    Evaluates required expressions

--- python-programming/dictionaries-and-tuples/comparing_different_data_types.py
+++ python-programming/dictionaries-and-tuples/comparing_different_data_types.py
@@ -1,1 +1,19 @@
+# Test the evaluation of the following operations
 
+a = 99 > 5
+print(a) #true
+
+b = 0 == False
+print(b) #true
+
+c = 1 == True
+print(c) #true
+
+d = 6.2 < 7
+print(d) #true
+
+e = 9 >= 9
+print(e) #true
+
+f = False < True
+print(f) #true
