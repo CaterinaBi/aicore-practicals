55c7f6638e3736e6a5250306f2e573ce824c4a20
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-16 21:44:31 +0100

    Solves problem

--- python-programming/dictionaries-and-tuples/xor-gate.py
+++ python-programming/dictionaries-and-tuples/xor-gate.py
@@ -4,11 +4,20 @@ B = False
 
 # Using logical operators, calculate:
 #The output of an OR logic gate (A or B) and store the value in C
+C = A or B
+print(C)
 
 # The output of an AND logic gate (A and B) and store the value in D
+D = A and B
+print(D)
 
 # The output of a NAND (not D) and store the value in D_NOT
+D_NOT = not D
+print(D_NOT)
 
 # Finally, the output of the XOR gate (C and D_NOT) and store it in XOR
+XOR = C and D_NOT
+print(XOR)
 
 # Can you do all the steps in one line?
+XOR = A or B and not A and B
