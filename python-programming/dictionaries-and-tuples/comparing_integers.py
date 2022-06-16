4b4956352bcb6d32675c9e17b362cf951c43a860
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-16 21:55:49 +0100

    Implements code

--- python-programming/dictionaries-and-tuples/comparing_integers.py
+++ python-programming/dictionaries-and-tuples/comparing_integers.py
@@ -1,1 +1,10 @@
+# You have one variable x = 10.
+x = 10
 
+# Use the function input() to ask the user to introduce a number and store it into n
+n = int(input("Please enter and integer: "))
+
+# Print "{n} is greater than {x}" if the introduced number is greater than x
+
+if n > x:
+    print(f"{n} is greater than {x}")
