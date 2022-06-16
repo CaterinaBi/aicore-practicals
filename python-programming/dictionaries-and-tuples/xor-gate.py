3589d981283a0ccb24134627a17c0582b62cb0ac
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-16 21:33:47 +0100

    Adds instructions

--- python-programming/dictionaries-and-tuples/xor-gate.py
+++ python-programming/dictionaries-and-tuples/xor-gate.py
@@ -1,1 +1,12 @@
+# Create two variables, A and B, whose values are True and False respectively (just to avoid confusion, you can hard code A = True and B = False)
 
+# Using logical operators, calculate:
+#The output of an OR logic gate (A or B) and store the value in C
+
+# The output of an AND logic gate (A and B) and store the value in D
+
+# The output of a NAND (not D) and store the value in D_NOT
+
+# Finally, the output of the XOR gate (C and D_NOT) and store it in XOR
+
+# Can you do all the steps in one line?
