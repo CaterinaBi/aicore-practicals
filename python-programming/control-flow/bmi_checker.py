3c3eb3d5bc30a27a71a7be612ea43bb63fadf48d
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-17 21:14:33 +0100

    Initialises file

--- python-programming/control-flow/bmi_checker.py
+++ python-programming/control-flow/bmi_checker.py
@@ -1,1 +1,13 @@
+# Given 2 parameters, height and weight, calculate the parameter BMI (Google the formula).
 
+# Using the parameter BMI, write a series of if statements that print the following outcomes:
+# below 18.5 --> 'Your BMI is x. You're in the underweight range.'
+# between 18.5 and 24.9 --> 'Your BMI is x. You're in the healthy weight range.'
+# between 25 and 29.9 --> 'Your BMI is x. You're in the overweight range.'
+# between 30 and 39.9 --> 'Your BMI is x. You're in the obese range.'
+
+
+# Test your code with the following cases by substituting them in for weight and height values in your code:
+# Height: 1.83m, Weight: 85kg
+# Height: 1.55m, Weight: 61kg
+# Height: 2.09m, Weight: 135kg
