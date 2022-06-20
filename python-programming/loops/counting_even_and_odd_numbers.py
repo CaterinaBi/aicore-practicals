2400ea0897b861c64c07fb352e72cd214f6df476
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-20 20:43:45 +0100

    Implements required functionalities

--- python-programming/loops/counting_even_and_odd_numbers.py
+++ python-programming/loops/counting_even_and_odd_numbers.py
@@ -1,3 +1,17 @@
 # Write a for loop to count the sum of all even numbers between 1 and 100.
+sum = 0
+
+for i in range(1,101):
+    if i % 2 == 0:
+        print(i)
+        sum = sum + i
+        print("sum =", sum)
 
 # Write another for loop to count the sum of all odd numbers between 1 and 100
+sum_2 = 0
+
+for i in range(1,101):
+    if i % 2 == 1:
+        print(i)
+        sum_2 = sum_2 + i
+        print("sum =", sum_2)
