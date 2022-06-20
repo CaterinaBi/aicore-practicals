3ffc7485b8088030b1a1f67c823b99000241cddd
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-20 20:20:56 +0100

    Implements to dos

--- python-programming/control-flow/flight_safety_checker.py
+++ python-programming/control-flow/flight_safety_checker.py
@@ -1,1 +1,13 @@
+# Given 2 parameters, altitude (ft) and airspeed (knots)
 
+# Write a program that categorises entries into 'safe flying' and 'unsafe flying' based on the following criteria:
+
+# An altitude below 100ft or above 50000ft is considered unsafe flying.
+# An airspeed below 60 knots or above 500 knots is considered unsafe flying.
+# If altitude and airspeed are outside these ranges, the flight is considered as safe.
+# CLUE: You will have to figure out the syntax for using and/or keywords in if statements
+
+# Try to write this as cleanly as possible and test your code with the following by substituting in again:
+# Altitude: 25000ft, Airspeed: 300 knots
+# Altitude: 50001ft, Airspeed: 250 knots
+# Altitude: 90ft, Airspeed: 125 knots
