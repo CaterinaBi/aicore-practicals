c1639810667f665f3374a4b9afc755de58d0d698
Caterina Bonan <97481648+CaterinaBi@users.noreply.github.com>
2022-06-28 13:43:38 +0100

    Adds instructions, variables, first functions

--- python-programming/loops/palindrome.py
+++ python-programming/loops/palindrome.py
@@ -1,1 +1,11 @@
+# Create a function which checks whether a word is a palindrome or not
 
+# asks for input
+word = input('Please enter a word to check whether it\'s a palyndrome: ')
+# print(word)
+
+# reverses a given word, word
+def my_function(word):
+    word2 = word[::-1]
+    return word2
+# print(my_function(word))
